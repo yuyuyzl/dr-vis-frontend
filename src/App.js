cc113ae24f3fde151c28ca17db9c5f9e2588a296
yuyuyzl <yuzhongliang22@163.COM>
2019-11-14 17:04:55 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import config from "./config";
+import { BrowserRouter, Route, Link,NavLink } from "react-router-dom";
+import Axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
