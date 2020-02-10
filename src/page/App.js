d577cd692afba9129f85da80f00767b265ec196e
yuyuyzl <yuzhongliang22@163.COM>
2020-02-11 02:09:32 +0800

    GA title

--- src/page/App.js
+++ src/page/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.less';
 import { BrowserRouter, Route, } from "react-router-dom";
+import Analytics from 'react-router-ga';
 import PatientPage from "../module/PatientPage";
 import Nav from "../module/Nav";
 import Search from "../module/Search";
@@ -9,15 +10,17 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Nav/>
-                <div className="container">
-                    <Route path={"/"} exact>
-                        <div className='wrapper-search'>
-                            <Search/>
-                        </div>
-                    </Route>
-                    <Route path={"/patient/:pdid"} render={route=><PatientPage pdid={route.match.params.pdid}/>}/>
-                </div>
+                <Analytics id="UA-74076320-5">
+                    <Nav/>
+                    <div className="container">
+                        <Route path={"/"} exact>
+                            <div className='wrapper-search'>
+                                <Search/>
+                            </div>
+                        </Route>
+                        <Route path={"/patient/:pdid"} render={route=><PatientPage pdid={route.match.params.pdid}/>}/>
+                    </div>
+                </Analytics>
             </BrowserRouter>
         </div>
     );
